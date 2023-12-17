0cddadefe2d09aeea8185a63e3186d648aa931b1
Yurii Baryliak <yuriibaryliak@Yuriis-MacBook-Pro.local>
2023-12-17 22:33:03 +0200

    Extend tailwind with custom color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
-const App = () => <>Yariga</>;
+const App = () => <h1>Yariga</h1>;
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#475BE8",
+        "text-primary-light": "#11142D",
+        "text-primary-dark": "#EFEFEF",
+        "text-secondary-light": "#808191",
+        "text-secondary-dark": "#6F767E",
+        black: "#1A1D1F",
+        white: "#FCFCFC",
+        "black-secondary": "#6F767E",
+        "bg-light": "#F4F4F4",
+        warning: "#FD8539",
+        success: "#2ED480",
+        secondary: "#CFC8FF",
+        danger: "#F45252",
+      },
+    },
   },
   plugins: [],
-}
-
+};
