3d138d27e81118de9606e6db7635020141d4eebf
Yurii Baryliak <yuriibaryliak@Yuriis-MacBook-Pro.local>
2023-12-18 14:36:17 +0200

    Add theme controller

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
-const App = () => <h1>Yariga</h1>;
+import ScreenContainer from "./components/ScreenContainer";
+import ThemeContextProvider from "./customization/ThemeContext";
+
+const App = () => (
+  <ThemeContextProvider>
+    <ScreenContainer>
+      <h1>Yariga</h1>
+    </ScreenContainer>
+  </ThemeContextProvider>
+);
 
 export default App;
