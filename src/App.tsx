57a2bdc5c0d2f33d5ad62037f28a997aa7beb798
Yurii Baryliak <yuriibaryliak@Yuriis-MacBook-Pro.local>
2024-03-17 19:09:41 +0200

    Add state persistence

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import ThemeContextProvider from "./customization/ThemeContext";
 import { RouterProvider } from "react-router-dom";
-import { store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
 import router from "./router";
 
 const App = () => (
   <Provider store={store}>
-    <ThemeContextProvider>
-      <RouterProvider router={router} />
-    </ThemeContextProvider>
+    <PersistGate persistor={persistor} loading={null}>
+      <ThemeContextProvider>
+        <RouterProvider router={router} />
+      </ThemeContextProvider>
+    </PersistGate>
   </Provider>
 );
 
