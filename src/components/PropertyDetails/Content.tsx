a15094f0a09d241953897b50e526cdf2947cfb14
Seibeaaar <yuribar4520@gmail.com>
2024-10-24 11:43:30 +0300

    Add property location map

--- src/components/PropertyDetails/Content.tsx
+++ src/components/PropertyDetails/Content.tsx
@@ -5,6 +5,7 @@ import PropertyGallery from '../Property/Gallery';
 import PropertyDetailsHeading from './Heading';
 import PropertyDetailsFacilities from './Facilities';
 import PropertyLandlordDetails from './LandlordDetails';
+import PropertyLocationMap from './LocationMap';
 
 type PropertyDetailsContentProps = {
   property: PropertyExtended;
@@ -15,19 +16,22 @@ const PropertyDetailsContent: FC<PropertyDetailsContentProps> = ({
 }) => {
   return (
     <section className="flex gap-[24px]">
-      <div className="flex flex-col gap-[24px]">
+      <div>
         <PropertyGallery photos={property.photos} />
-        <PropertyDetailsHeading property={property} />
-        <PropertyDetailsFacilities facilities={property.facilities} />
-        <div>
-          <h6 className="text-lg font-medium mb-[8px]">Description</h6>
-          <p className="text-sm text-secondary-light dark:text-secondary-dark">
-            {property.description}
-          </p>
+        <div className="flex flex-col gap-[24px] mt-[24px] mr-[48px]">
+          <PropertyDetailsHeading property={property} />
+          <PropertyDetailsFacilities facilities={property.facilities} />
+          <div>
+            <h6 className="text-lg font-medium mb-[8px]">Description</h6>
+            <p className="text-sm text-secondary-light dark:text-secondary-dark">
+              {property.description}
+            </p>
+          </div>
         </div>
       </div>
-      <div className="w-[40%]">
+      <div className="w-[40%] flex flex-col gap-[20px]">
         <PropertyLandlordDetails landlord={property.owner} />
+        <PropertyLocationMap location={property.location} />
       </div>
     </section>
   );
